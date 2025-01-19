a0c5eb8b41c0e455ebde4803e02b8ac6e8091217
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-01-19 19:13:25 -0300

    fix: update setup script to prevent duplicate folders

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 # Neovim symlink
-if [ -e ~/.config/nvim ]; then
+if [ -L ~/.config/nvim ]; then
+  echo "Removing existing Neovim symlink..."
+  rm ~/.config/nvim
+elif [ -e ~/.config/nvim ]; then
   echo "Backing up existing nvim config..."
   mv ~/.config/nvim ~/.config/nvim.backup
 fi
@@ -7,7 +10,10 @@ ln -s ~/.dotfiles/nvim ~/.config/nvim
 echo "Symlink created for Neovim."
 
 # tmux symlink
-if [ -e ~/.tmux.conf ]; then
+if [ -L ~/.tmux.conf ]; then
+  echo "Removing existing tmux symlink..."
+  rm ~/.tmux.conf
+elif [ -e ~/.tmux.conf ]; then
   echo "Backing up existing tmux.conf..."
   mv ~/.tmux.conf ~/.tmux.conf.backup
 fi
@@ -15,20 +21,26 @@ ln -s ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
 echo "Symlink created for tmux."
 
 # zsh symlink
-if [ -e ~/.zshrc ]; then
+if [ -L ~/.zshrc ]; then
+  echo "Removing existing zsh symlink..."
+  rm ~/.zshrc
+elif [ -e ~/.zshrc ]; then
   echo "Backing up existing zshrc..."
   mv ~/.zshrc ~/.zshrc.backup
 fi
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 echo "Symlink created for zsh."
 
-# zsh symlink
-if [ -e ~/.fonts ]; then
+# fonts symlink
+if [ -L ~/.fonts ]; then
+  echo "Removing existing fonts symlink..."
+  rm ~/.fonts
+elif [ -e ~/.fonts ]; then
   echo "Backing up existing fonts..."
   mv ~/.fonts ~/.fonts.backup
 fi
 ln -s ~/.dotfiles/fonts/.fonts/ ~/.fonts
 echo "Symlink created for fonts."
 
-
 echo "All done!"
+

7a3f42c8365cceb0d6ae6f7b552389227bba835f
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-01-19 19:05:56 -0300

    feat: add goimport to be used of format files .go

--- nvim/lua/edu-gomes/plugins/autoformat.lua
+++ nvim/lua/edu-gomes/plugins/autoformat.lua
@@ -33,9 +33,7 @@ return { -- Autoformat
 		end,
 		formatters_by_ft = {
 			lua = { "stylua" },
-			-- Conform can also run multiple formatters sequentially
-			-- python = { "isort", "black" },
-			--
+			go = { "goimports" },
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },

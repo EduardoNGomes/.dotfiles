bdbd3212fc81dd9b7a29c83d64904be07fd58e13
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-02-24 19:21:09 -0300

    feat:update nv config

--- nvim/lua/edu-gomes/plugins/colors.lua
+++ nvim/lua/edu-gomes/plugins/colors.lua
@@ -4,7 +4,7 @@ local function set_transparent_bg()
 end
 
 function ChangeTheme(theme)
-	theme = theme or "catppuccin"
+	theme = theme or "tokyonight-night"
 
 	if theme == "davinci" then
 		require("edu-gomes.themes.davinci").setup()
@@ -35,20 +35,15 @@ vim.keymap.set("n", "<Leader>t5", function()
 	ChangeTheme("tokyonight-night")
 end, { desc = "Switch to Tokyo Night" })
 
+vim.keymap.set("n", "<Leader>t6", function()
+	ChangeTheme("leaf")
+end, { desc = "Switch to Leaf" })
+
 return {
-	{
-		"baliestri/aura-theme",
-		priority = 50,
-		lazy = false,
-		config = function(plugin)
-			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-			vim.cmd([[colorscheme aura-dark]])
-			set_transparent_bg()
-		end,
-	},
 	-- TokyoNight theme setup
 	{
 		"folke/tokyonight.nvim",
+		priority = 1000,
 		init = function()
 			vim.cmd.colorscheme("tokyonight-night")
 			vim.g.tokyonight_transparent = true
@@ -62,9 +57,41 @@ return {
 					sidebars = "transparent",
 					floats = "transparent",
 				},
+				on_colors = function() end, -- Empty function
+				on_highlights = function() end, -- Empty function
+				style = "night", -- Default style
+				light_style = "day", -- Default light style
+				terminal_colors = true, -- Default value
+				day_brightness = 0.3, -- Default value
+				dim_inactive = false, -- Default value
+				lualine_bold = false, -- Default value
+				cache = true, -- Default value
+				plugins = {
+					-- Add empty plugin configurations
+					cmp = true,
+					gitsigns = true,
+					indent_blankline = true,
+					nvim_tree = true,
+					telescope = true,
+					treesitter = true,
+					which_key = true,
+				},
 			})
 		end,
 	},
+
+	-- Aura theme setup
+	{
+		"baliestri/aura-theme",
+		priority = 50,
+		lazy = false,
+		config = function(plugin)
+			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+			vim.cmd([[colorscheme aura-dark]])
+			set_transparent_bg()
+		end,
+	},
+
 	-- Onedarkpro theme
 	{
 		"olimorris/onedarkpro.nvim",
@@ -85,13 +112,35 @@ return {
 	{
 		"catppuccin/nvim",
 		name = "catppuccin",
-		priority = 1000,
+		priority = 10,
 		config = function()
 			require("catppuccin").setup({
 				flavour = "mocha",
 				transparent_background = true,
 			})
 			ChangeTheme("catppuccin")
+		end,
+	},
+
+	{
+		"daschw/leaf.nvim",
+		priority = 100,
+		config = function()
+			require("leaf").setup({
+				underlineStyle = "underline",
+				commentStyle = "italic",
+				functionStyle = "NONE",
+				keywordStyle = "italic",
+				statementStyle = "bold",
+				typeStyle = "NONE",
+				variablebuiltinStyle = "italic",
+				transparent = true,
+				colors = {},
+				overrides = {},
+				theme = "auto", -- default, based on vim.o.background, alternatives: "light", "dark"
+				contrast = "high", -- default, alternatives: "medium", "high"
+			})
+			ChangeTheme("leaf")
 		end,
 	},
 }

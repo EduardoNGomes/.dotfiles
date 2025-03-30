737b08290e4a05b965738de0e09982af6e50c39e
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-03-30 14:07:29 -0300

    styles:config nv theme

--- nvim/lua/edu-gomes/plugins/colors.lua
+++ nvim/lua/edu-gomes/plugins/colors.lua
@@ -1,44 +1,3 @@
-local function set_transparent_bg()
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-function ChangeTheme(theme)
-	theme = theme or "tokyonight-night"
-
-	if theme == "davinci" then
-		require("edu-gomes.themes.davinci").setup()
-	else
-		vim.cmd.colorscheme(theme)
-	end
-
-	set_transparent_bg()
-end
-
-vim.keymap.set("n", "<Leader>t1", function()
-	ChangeTheme("aura-dark")
-end, { desc = "Switch to aura-dark" })
-
-vim.keymap.set("n", "<Leader>t2", function()
-	ChangeTheme("catppuccin")
-end, { desc = "Switch to Catppuccin" })
-
-vim.keymap.set("n", "<Leader>t3", function()
-	ChangeTheme("onedark")
-end, { desc = "Switch to Onedark" })
-
-vim.keymap.set("n", "<Leader>t4", function()
-	ChangeTheme("davinci")
-end, { desc = "Switch to Davinci" })
-
-vim.keymap.set("n", "<Leader>t5", function()
-	ChangeTheme("tokyonight-night")
-end, { desc = "Switch to Tokyo Night" })
-
-vim.keymap.set("n", "<Leader>t6", function()
-	ChangeTheme("leaf")
-end, { desc = "Switch to Leaf" })
-
 return {
 	-- TokyoNight theme setup
 	{
@@ -46,9 +5,6 @@ return {
 		priority = 1000,
 		init = function()
 			vim.cmd.colorscheme("tokyonight-night")
-			vim.g.tokyonight_transparent = true
-			vim.cmd.colorscheme("tokyonight-night")
-			vim.cmd.hi("Comment gui=none")
 		end,
 		config = function()
 			require("tokyonight").setup({
@@ -77,70 +33,6 @@ return {
 					which_key = true,
 				},
 			})
-		end,
-	},
-
-	-- Aura theme setup
-	{
-		"baliestri/aura-theme",
-		priority = 50,
-		lazy = false,
-		config = function(plugin)
-			vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-			vim.cmd([[colorscheme aura-dark]])
-			set_transparent_bg()
-		end,
-	},
-
-	-- Onedarkpro theme
-	{
-		"olimorris/onedarkpro.nvim",
-		name = "onedarkpro",
-		priority = 50,
-		config = function()
-			require("onedarkpro").setup({
-				options = {
-					transparency = true, -- Enable transparent background
-				},
-			})
-			ChangeTheme("onedark")
-			set_transparent_bg()
-		end,
-	},
-
-	-- Catppuccin theme setup
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 10,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha",
-				transparent_background = true,
-			})
-			ChangeTheme("catppuccin")
-		end,
-	},
-
-	{
-		"daschw/leaf.nvim",
-		priority = 100,
-		config = function()
-			require("leaf").setup({
-				underlineStyle = "underline",
-				commentStyle = "italic",
-				functionStyle = "NONE",
-				keywordStyle = "italic",
-				statementStyle = "bold",
-				typeStyle = "NONE",
-				variablebuiltinStyle = "italic",
-				transparent = true,
-				colors = {},
-				overrides = {},
-				theme = "auto", -- default, based on vim.o.background, alternatives: "light", "dark"
-				contrast = "high", -- default, alternatives: "medium", "high"
-			})
-			ChangeTheme("leaf")
 		end,
 	},
 }

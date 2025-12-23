5049382beb58dd160472fc2f274810790f29464c
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2025-12-22 23:00:46 -0300

    feat: ensure `swww-daemon` is running before setting wallpaper

--- scripts/wallpaper.sh
+++ scripts/wallpaper.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
 WALLPAPER_DIR="$HOME/.dotfiles/wallpaper"
-INTERVAL=300 
+INTERVAL=300
 
-swww query || swww-daemon --format xrgb
+if ! swww query &> /dev/null; then
+    swww-daemon --format xrgb &
+    wait $!
+    sleep 1 
+fi
 
 while true; do
     RANDOM_IMG=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
-    swww img "$RANDOM_IMG" \
-        --transition-type wave \
-        --transition-fps 144 \
-        --transition-duration 2 \
-        --transition-angle 270 \
-        --transition-wave 50,25
+    if [[ -n "$RANDOM_IMG" ]]; then
+        swww img "$RANDOM_IMG" \
+            --transition-type wave \
+            --transition-fps 144 \
+            --transition-duration 2 \
+            --transition-angle 270 \
+            --transition-wave 50,25
+    fi
+
     sleep $INTERVAL
 done

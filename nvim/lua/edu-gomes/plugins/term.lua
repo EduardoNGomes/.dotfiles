04ec5d3cc382b97789ec801c5f91ba2e078f725e
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-01-15 13:42:35 -0300

    feat: add keymaps for toggling tmux and codex terminals

--- nvim/lua/edu-gomes/plugins/term.lua
+++ nvim/lua/edu-gomes/plugins/term.lua
@@ -10,6 +10,31 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<leader>T", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+		local Terminal = require("toggleterm.terminal").Terminal
+
+		local tmux = Terminal:new({
+			cmd = "tmux",
+			hidden = true,
+			direction = "float",
+		})
+
+		local function toggle_tmux()
+			tmux:toggle()
+		end
+
+		local codex = Terminal:new({
+			cmd = "codex",
+			hidden = true,
+			direction = "vertical",
+		})
+
+		local function toggle_codex()
+			local width = vim.o.columns * 0.4
+
+			codex:toggle(width, "vertical")
+		end
+
+		vim.keymap.set("n", "<leader>T", toggle_tmux, { desc = "Toggle Terminal (Tmux)" })
+		vim.keymap.set("n", "<leader>tc", toggle_codex, { desc = "Toggle Codex (Side)" })
 	end,
 }

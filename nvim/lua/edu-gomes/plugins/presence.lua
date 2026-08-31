9f97040b4359a31b834a12f09f787732f927628a
Eduardo Gomes <eduardo_n_gomes.dev@hotmail.com>
2026-08-31 07:31:24 -0300

    feat(nvim): customize discord presence

--- nvim/lua/edu-gomes/plugins/presence.lua
+++ nvim/lua/edu-gomes/plugins/presence.lua
@@ -4,13 +4,16 @@ return {
 		require("presence").setup({
 			auto_update = true,
 			main_image = "file",
-			enable_line_number = true,
+			enable_line_number = false,
 			show_time = true,
 			workspace_text = "Working on a project",
 			file_explorer_text = "Browsing Filetree",
 			editing_text = function(filename)
 				local ext = filename:match("^.+(%..+)$") or ""
-				return "Editing a " .. ext .. " file"
+
+				return "Writing unbelievable things in " .. ext .. " "
+
+				------------------return "Editing a " .. ext .. " file"
 			end,
 		})
 	end,
